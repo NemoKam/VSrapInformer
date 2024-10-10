222c9721c28785b72d1b848488c00ba4da7ae1ba
Kamil Habibullin <kamilhabibullin@MacBook-Air-3.local>
2024-10-10 13:40:13 +0300

    Added webapp

--- fastapp/fast.py
+++ fastapp/fast.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordBearer
 
-from fastapp.api.routes import router as main_router
-
-from core import config
-from sqlalchemyapp import models
+from fastapp import models
+from fastapp.core import config
+from fastapp.api.routes import router as api_router
 from fastapp.database import engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -14,4 +17,16 @@ app = FastAPI(
     openapi_url="/api/openapi.json",
 )
 
-app.include_router(main_router, prefix="/api")
+app.include_router(api_router, prefix="/api")
+
+# load static and templates
+app.mount("/static", StaticFiles(directory="fastapp/static"), name="static")
+templates = Jinja2Templates(directory="fastapp/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_main(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="main.html", context={'title': app.title}
+    )
+
+
